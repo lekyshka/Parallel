8e1b36b473d20e471c0bd49f5bbf757b31426655
Alena Bannikova <145114139+lekyshka@users.noreply.github.com>
2024-05-04 17:07:07 +0700

    Update main.cpp

--- task6/task_gpu/main.cpp
+++ task6/task_gpu/main.cpp
@@ -17,11 +17,6 @@ namespace po = boost::program_options;
 void initialize(double *A, double *Anew, int n)
 {
     memset(A, 0, n * n * sizeof(double));
-    memset(Anew, 0, n *n * sizeof(double));
-    // for(int i = 0; i < n * n; i++){
-    //     A[i] = 0.0;
-    //     Anew[i] = 0.0;
-    // }
     double corners[4] = {10, 20, 30, 20};
     A[0] = corners[0];
     A[n - 1] = corners[1];
@@ -35,11 +30,8 @@ void initialize(double *A, double *Anew, int n)
         A[n * i] = corners[0] + i * step;
         A[(n-1) + n * i] = corners[1] + i * step;
         A[n * (n-1) + i] = corners[3] + i * step;
-        Anew[i] = corners[0] + i * step;
-        Anew[n * i] = corners[0] + i * step;
-        Anew[(n-1) + n * i] = corners[1] + i * step;
-        Anew[n * (n-1) + i] = corners[3] + i * step;
     }
+    std::memcpy(Anew, A, n * n * sizeof(double));
     #pragma acc enter data copyin(A[:n*n],Anew[:n*n])
 }
 
@@ -51,40 +43,21 @@ double calcNext(double *A, double *Anew, int n)
     for( int j = 1; j < n-1; j++) {
         #pragma acc loop
         for( int i = 1; i < n-1; i++ ) {
-            // Anew[OFFSET(j, i, n)] = ( A[OFFSET(j, i+1, n)] + A[OFFSET(j, i-1, n)]
-            //                             + A[OFFSET(j-1, i, n)] + A[OFFSET(j+1, i,n)])*0.25;
-            // error = fmax( error, fabs(Anew[OFFSET(j, i, n)] - A[OFFSET(j, i , n)]));
-
-            Anew[i * n + j] = ( A[i * n + j - 1] + A[(i - 1) * n + j]
-                                        + A[(i + 1) * n + j] + A[i * n + j + 1])*0.25;
-            error = fmax(error, Anew[i * n + j] - A[i * n + j]);
-
-
+            Anew[OFFSET(j, i, n)] = ( A[OFFSET(j, i+1, n)] + A[OFFSET(j, i-1, n)]
+                                        + A[OFFSET(j-1, i, n)] + A[OFFSET(j+1, i,n)])*0.25;
+            error = fmax( error, fabs(Anew[OFFSET(j, i, n)] - A[OFFSET(j, i , n)]));
         }
     }
-
 
     return error;
 }
-        
-void swap(double *A, double *Anew, int n)
-{
-    #pragma acc parallel loop present(A,Anew)
-    for( int j = 1; j < n-1; j++)
-    {
-        #pragma acc loop
-        for( int i = 1; i < n-1; i++ )
-        {
-            A[OFFSET(j, i, n)] = Anew[OFFSET(j, i, n)];    
-        }
-    }
-}
+
 
 void deallocate(double *A, double *Anew)
 {
     #pragma acc exit data delete(A,Anew)
-    free(A);
-    free(Anew);
+    delete[] A;
+    delete[] Anew;
 }
 
 
@@ -93,7 +66,7 @@ int main(int argc, char* argv[]) {
     desc.add_options()
         ("help", "Produce help message")
         ("precision", po::value<double>()->default_value(0.000001), "Set precision")
-        ("grid-size", po::value<int>()->default_value(10), "Set grid size")
+        ("grid-size", po::value<int>()->default_value(1024), "Set grid size")
         ("iterations", po::value<int>()->default_value(1000000), "Set number of iterations");
 
     po::variables_map vm;
@@ -111,8 +84,8 @@ int main(int argc, char* argv[]) {
 
     double error = 1.0;
 
-    double *A = (double *)malloc(sizeof(double) * n * n);
-    double *Anew = (double *)malloc(sizeof(double) * n * n);
+    double* A = new double[n * n];
+    double* Anew = new double[n * n];
 
     initialize(A, Anew, n);
 
@@ -120,14 +93,31 @@ int main(int argc, char* argv[]) {
 
     double st = omp_get_wtime();
     int iter = 0;
-
+    #pragma acc data copyin(error)
     while (error > precision && iter < iter_max)
     {
-        error = calcNext(A, Anew, n);
+       if(iter % 100 == 0){
+            error = 0.0;
+            #pragma acc update device(error) async(1)
+        }
+        #pragma acc parallel loop reduction(max:error) present(A,Anew)
+        for( int j = 1; j < n-1; j++) {
+            #pragma acc loop
+            for( int i = 1; i < n-1; i++ ) {
+                Anew[OFFSET(j, i, n)] = ( A[OFFSET(j, i+1, n)] + A[OFFSET(j, i-1, n)]
+                                            + A[OFFSET(j-1, i, n)] + A[OFFSET(j+1, i,n)])*0.25;
+                error = fmax( error, fabs(Anew[OFFSET(j, i, n)] - A[OFFSET(j, i , n)]));
+            }
+        }
+       
+        double* temp = A;
+        A = Anew;
+        Anew = temp;
 
-        swap(A, Anew, n);
 
         if (iter % 100 == 0)
+        	#pragma acc update host(error) async(1)	
+	        #pragma acc wait(1)
             printf("%5d, %0.6f\n", iter, error);
 
         iter++;
@@ -138,13 +128,7 @@ int main(int argc, char* argv[]) {
     printf("%5d, %0.6f\n", iter, error);
 
     printf(" total: %f s\n", runtime);
-    #pragma acc update host(A[:n*n]) 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            printf("%0.6f ", A[i*n+j]); 
-        }
-        printf("\n");
-    }
+
 
     deallocate(A, Anew);
  
