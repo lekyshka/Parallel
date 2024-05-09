bde952c188399474c51c559c40fdcf816b3779a1
Alena Bannikova <145114139+lekyshka@users.noreply.github.com>
2024-05-09 18:44:44 +0700

    Update main.cpp

--- task6/task_gpu/main.cpp
+++ task6/task_gpu/main.cpp
@@ -84,7 +84,7 @@ int main(int argc, char* argv[]) {
         if(iter % 1000 == 0){
                 error = 0.0;
                 #pragma acc update device(error) async(1)
-                #pragma acc parallel loop independent collapse(2) vector vector_length(256) gang num_gangs(n) reduction(max:error) present(A,Anew)
+                #pragma acc parallel loop independent collapse(2) vector vector_length(n) gang num_gangs(n) reduction(max:error) present(A,Anew)
                 for( int j = 1; j < n-1; j++) {
                     for( int i = 1; i < n-1; i++ ) {
                         Anew[OFFSET(j, i, n)] = ( A[OFFSET(j, i+1, n)] + A[OFFSET(j, i-1, n)]
