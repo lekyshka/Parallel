cbb9900c186de35ad8acfb113456420a1989b114
Alena Bannikova <145114139+lekyshka@users.noreply.github.com>
2024-05-14 13:22:59 +0700

    Update main.cpp

--- task7/main.cpp
+++ task7/main.cpp
@@ -4,6 +4,7 @@
 
 namespace po = boost::program_options;
 
+
 #include <cublas_v2.h>
 
 #include <memory>
@@ -11,6 +12,7 @@ namespace po = boost::program_options;
 #include <fstream>
 #include <iomanip>
 #include <chrono>
+
 #define OFFSET(x, y, m) (((x)*(m)) + (y))
 
 void initialize(std::unique_ptr<double[]> &A, std::unique_ptr<double[]> &Anew, int n)
@@ -50,16 +52,15 @@ int main(int argc, char* argv[]) {
     desc.add_options()
         ("help", "Produce help message")
         ("precision", po::value<double>()->default_value(0.000001), "Set precision")
-        ("grid-size", po::value<int>()->default_value(128), "Set grid size")
+        ("grid-size", po::value<int>()->default_value(10), "Set grid size")
         ("iterations", po::value<int>()->default_value(1000000), "Set number of iterations");
 
     po::variables_map vm;
-
-    po::store(po::parse_command_line(argc, argv, desc), vm);
     if (vm.count("help")) {
         std::cout << desc << std::endl;
         return 1;
     }
+    po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
 
 
@@ -79,24 +80,23 @@ int main(int argc, char* argv[]) {
     printf("Jacobi relaxation Calculation: %d x %d mesh\n", n, n);
 
 
-    
+    cublasHandle_t handler;
+	cublasStatus_t status;
+	
 	double error = 1.0;
 	int iter = 0, idx = 0;
-  double alpha = -1.0;
-  cublasHandle_t handler;
-	cublasStatus_t status;
-	status = cublasCreate(&handler);
+	double alpha = -1.0;
 
+	status = cublasCreate(&handler);
     if (status != CUBLAS_STATUS_SUCCESS) {
         std::cerr << "cublasCreate failed with error code: " << status << std::endl;
         return 13;
     }
-
-    #pragma acc data copyin(A[:n*n],Anew[:n*n], idx, n, alpha)
+    #pragma acc data copyin(A[:n*n],Anew[:n*n], alpha, n, idx)
     {
         auto start = std::chrono::high_resolution_clock::now();
+         
 
-	
         while (error > precision && iter < iter_max)
         {
             #pragma acc parallel loop independent collapse(2) vector vector_length(n) gang num_gangs(n) present(A,Anew)
@@ -110,31 +110,34 @@ int main(int argc, char* argv[]) {
 
 
             if (iter % 100 == 0){
+                
                 #pragma acc data present (A, Anew) wait
                 #pragma acc host_data use_device(A, Anew)
                 {
 
                     status = cublasDaxpy(handler, n * n, &alpha, Anew, 1, A, 1);
+                
                     if (status != CUBLAS_STATUS_SUCCESS) {
                         std::cerr << "cublasDaxpy failed with error code: " << status << std::endl;
-                        return 13;
+                        exit (13);
                     }
 
+                        
                     status = cublasIdamax(handler, n * n, A, 1, &idx);
                     if (status != CUBLAS_STATUS_SUCCESS) {
                         std::cerr << "cublasIdamax failed with error code: " << status << std::endl;
-                        return 13;
+                        exit (13);
                     }
                 }
 
                 #pragma acc update host(A[idx - 1]) 
                 error = std::fabs(A[idx - 1]);
-		
+	
                 #pragma acc host_data use_device(A, Anew)
                 status = cublasDcopy(handler, n * n, Anew, 1, A, 1);
                 if (status != CUBLAS_STATUS_SUCCESS) {
                     std::cerr << "cublasDcopy failed with error code: " << status << std::endl;
-                    return 13;
+                    exit (13);
                 }
                 printf("%5d, %0.6f\n", iter, error);
 			}
